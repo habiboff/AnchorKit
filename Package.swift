aeec95e6e18368e2b3addb7675edea668fb87e0f
byhabiboff <byhabiboff@gmail.com>
2024-01-27 21:18:32 +0400

    Remev test and multiole targets

--- Package.swift
+++ Package.swift
@@ -14,10 +14,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "AnchorKit"),
-        .testTarget(
-            name: "AnchorKitTests",
-            dependencies: ["AnchorKit"]),
+        .binaryTarget(name: "AnchorKit", path: "./Sources/AnchorKit.xcframework")
     ]
 )
---
+++ Sources/AnchorKit.xcframework/ios-arm64/AnchorKit.framework/Headers/AnchorKit.h
@@ -0,0 +1,19 @@
+//
+//  AnchorKit.h
+//  AnchorKit
+//
+//  Created by Nahit Habibov on 27.01.24.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnchorKit.
+FOUNDATION_EXPORT double AnchorKitVersionNumber;
+
+//! Project version string for AnchorKit.
+FOUNDATION_EXPORT const unsigned char AnchorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnchorKit/PublicHeader.h>
+
+
